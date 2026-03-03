4bdf90a61a0370d049b17f2c05957467e50f0f52
moyola1 <moyola1@hotmail.com>
2026-03-02 22:40:35 -0500

    add resume site config to terraform - typo with interpolation

--- tfdocs/main.tf
+++ tfdocs/main.tf
@@ -26,7 +26,7 @@ resource "aws_s3_bucket_policy" "myresume_policy" {
         Effect    = "Allow"
         Principal = "*"
         Action    = "s3:GetObject"
-        Resource  = "{aws_s3_bucket.myresume.arn}/*"
+        Resource  = "${aws_s3_bucket.myresume.arn}/*"
       }
     ]
   })
